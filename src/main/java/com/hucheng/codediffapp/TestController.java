e1549d2c799f026e184e834c54821441622f599d
qahucheng <chenghu95@163.com>
2023-08-23 22:39:46 +0800

    fix

--- src/main/java/com/hucheng/codediffapp/TestController.java
+++ src/main/java/com/hucheng/codediffapp/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-    @GetMapping
+    @GetMapping("test1/hello1")
     @ResponseBody
     public String hello1(String name) {
         return "hello1 : " + name;
