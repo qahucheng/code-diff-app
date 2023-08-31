afe6c022056c2ca728be3b57717707759c34a466
qahucheng <chenghu95@163.com>
2023-08-31 22:36:07 +0800

    ocr api 优化

--- src/main/java/com/hucheng/codediffapp/openapi/OpenApiController.java
+++ src/main/java/com/hucheng/codediffapp/openapi/OpenApiController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
 import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.sourceforge.tess4j.*;
 
@@ -23,29 +25,48 @@ public class OpenApiController {
         String base64String = jsonObject.getString("base64String");
         log.info("base64String字符串{}", base64String);
         File imageFile = new File("captcha_image.png"); // 临时保存图片文件
-        try {
-            // 解码Base64字符串为字节数组
-            byte[] imageBytes = Base64.getDecoder().decode(base64String);
+        byte[] imageBytes = Base64.getDecoder().decode(base64String);
 
-            try (OutputStream outputStream = new FileOutputStream(imageFile)) {
-                outputStream.write(imageBytes);
-            }
-            // 使用Tesseract进行验证码识别
-            ITesseract tesseract = new Tesseract();
-            tesseract.setLanguage("chi_sim");
-            tesseract.setDatapath("/opt/server/test-app/tessdata/tessdata");
-            captchaText = tesseract.doOCR(imageFile);
-            log.info("OCR识别成功{}:", captchaText);
-        } catch (Exception e) {
-            log.error("OCR识别失败{}", e.getMessage());
-        } finally {
-            // 删除临时图片文件
-            imageFile.delete();
+        try (OutputStream outputStream = new FileOutputStream(imageFile)) {
+            outputStream.write(imageBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        //最大次数5次
+        int count = 5;
+        while (captchaText.length() != 4 && count > 0) {
+            count--;
+            try {
+                // 使用Tesseract进行验证码识别
+                ITesseract tesseract = new Tesseract();
+                tesseract.setLanguage("eng");
+                tesseract.setDatapath("/opt/server/test-app/tessdata/tessdata");
+                captchaText = extractAlphanumeric(tesseract.doOCR(imageFile));
+                log.info("OCR识别成功{}:", captchaText);
+            } catch (TesseractException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 删除临时图片文件
+        imageFile.delete();
+
         JSONObject result = new JSONObject();
         result.put("code", captchaText);
         return result;
-
-
     }
+
+    private String extractAlphanumeric(String input) {
+        // 使用正则表达式匹配字母和数字
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9]+");
+        Matcher matcher = pattern.matcher(input);
+
+        StringBuilder sb = new StringBuilder();
+        while (matcher.find()) {
+            sb.append(matcher.group());
+        }
+
+        return sb.toString();
+    }
+
 }
