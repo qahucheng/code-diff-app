5770f3f020e837170006148f076f6a4ab5e5ff61
qahucheng <125987910+qahucheng@users.noreply.github.com>
2024-01-31 19:56:42 +0800

    Update TestController.java
    二次更新test1/hello2

--- src/main/java/com/hucheng/codediffapp/TestController.java
+++ src/main/java/com/hucheng/codediffapp/TestController.java
@@ -16,6 +16,6 @@ public class TestController {
     @GetMapping("test1/hello2")
     @ResponseBody
     public String hello2(String name) {
-        return "hello2.2 : " + name;
+        return "hello2.3 : " + name;
     }
 }
