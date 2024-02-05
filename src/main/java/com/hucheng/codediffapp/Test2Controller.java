6831cc87b2e09038093282770a8f8c0307c5f287
qahucheng <125987910+qahucheng@users.noreply.github.com>
2024-02-05 14:26:17 +0800

    Update

--- src/main/java/com/hucheng/codediffapp/Test2Controller.java
+++ src/main/java/com/hucheng/codediffapp/Test2Controller.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Test2Controller {
 
-    @GetMapping("test2/hello")
+    @GetMapping("test2/hello1")
+    @ResponseBody
+    public String hello(String name) {
+        if (name.startsWith("hucheng1995")) {
+            return "hello!!!  hucheng1995";
+        } else {
+            return "sorry!!!  hucheng1995";
+        }
+    }
+
+    @GetMapping("test2/hello2")
     @ResponseBody
     public String hello(String name) {
         if (name.startsWith("chenghu")){
@@ -15,9 +25,10 @@ public class Test2Controller {
         }
         if (name.startsWith("hucheng1995")) {
             return "hello!!!  hucheng1995";
-        } else {
-            return "sorry!!!  hucheng1995";
-        }
+        } 
+        return "default!!!  hucheng1995";
+        
     }
+
 
 }
